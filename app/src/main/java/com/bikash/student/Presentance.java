e6d0b249d14720da805268af56ea20376f938d3b
Bikash <bikash11roy@gmail.com>
2016-09-05 03:37:21 +0600

    Percentage Functionality

--- app/src/main/java/com/bikash/student/Presentance.java
+++ app/src/main/java/com/bikash/student/Presentance.java
@@ -4,42 +4,49 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class Presentance extends AppCompatActivity {
     public Teacher t;
-    public TextView t1_n;
-    public TextView t1_p;
+    public TextView s1_name;
+    public TextView s1_percentage;
+    public TextView s1_count;
 
+    //Adding functionality for the button "+" id = Present
     public void presentButton(View view){
         t.total_class++;
         t.present_classs++;
 
         int per = (t.present_classs*100)/t.total_class;
-        t1_p.setText(Integer.toString(per) + "%");
+        s1_percentage.setText(Integer.toString(per) + "%");
+        s1_count.setText(t.present_classs+"/"+t.total_class);
 
 
         if(per>=60){
-            t1_p.setBackgroundColor(Color.parseColor("#379237"));
+            s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
         } else {
-            t1_p.setBackgroundColor(Color.parseColor("#ff0000"));
+            s1_percentage.setBackgroundColor(Color.parseColor("#ff0000"));
         }
     }
 
+
+    //Adding functionality for the button "-" id = Basent
     public void absentButton(View view){
         t.total_class++;
 
         int per = (t.present_classs*100)/t.total_class;
-        t1_p.setText(Integer.toString(per) + "%");
+        s1_percentage.setText(Integer.toString(per) + "%");
+        s1_count.setText(t.present_classs+"/"+t.total_class);
 
 
         if(per>60){
-            t1_p.setBackgroundColor(Color.parseColor("#379237"));
+            s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
         } else {
-            t1_p.setBackgroundColor(Color.parseColor("#ff0000"));
+            s1_percentage.setBackgroundColor(Color.parseColor("#ff0000"));
         }
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,23 +54,22 @@ public class Presentance extends AppCompatActivity {
         setContentView(R.layout.activity_presentance);
 
         //Making a teacher Object
-        t  = new Teacher("Rana Sir",1,1);
+        t  = new Teacher("Advanced Algorithm",0,0);
 
 
-        //Getting textview bt its id
-        t1_n = (TextView)findViewById(R.id.t1);
-        t1_n.setText(t.name);
-
-        t1_p = (TextView)findViewById(R.id.t1p);
-        int per = (t.present_classs*100)/t.total_class;
-        t1_p.setText(Integer.toString(per) + "%");
+        //Getting textview for all ids bt its id
+        s1_name = (TextView)findViewById(R.id.t1);  //Name of Teachar 1 or Subject 1
+        s1_percentage = (TextView)findViewById(R.id.t1p); //Subject 1 Percentage count for Teacher 1
+        s1_count = (TextView)findViewById(R.id.count_1);
 
 
-        if(per>60){
-            t1_p.setBackgroundColor(Color.parseColor("#379237"));
-        } else {
-            t1_p.setBackgroundColor(Color.parseColor("#ff0000"));
-        }
+
+        //Working with Subject 1
+        s1_name.setText(t.name);
+        s1_percentage.setText( "0%");
+        s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
+
+
 
 
     }

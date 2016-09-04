661518b4fe20b993cbbabb7a32ac66f3deac605f
Bikash <bikash11roy@gmail.com>
2016-09-05 04:11:50 +0600

    Percentage Functionality

--- app/src/main/java/com/bikash/student/Presentance.java
+++ app/src/main/java/com/bikash/student/Presentance.java
@@ -7,42 +7,43 @@ import android.view.View;
 import android.widget.TextView;
 
 public class Presentance extends AppCompatActivity {
-    public Teacher t;
-    public TextView s1_name;
-    public TextView s1_percentage;
-    public TextView s1_count;
+    int n = 5;
+    public Subject[] s = new Subject[5];
+    public TextView[] sub_name = new TextView[5];
+    public TextView[] sub_percentage = new TextView[5];
+    public TextView[] sub_count = new TextView[5];
 
     //Adding functionality for the button "+" id = Present
     public void presentButton(View view){
-        t.total_class++;
-        t.present_classs++;
+        s[1].total_class++;
+        s[1].present_classs++;
 
-        int per = (t.present_classs*100)/t.total_class;
-        s1_percentage.setText(Integer.toString(per) + "%");
-        s1_count.setText(t.present_classs+"/"+t.total_class);
+        int per = (s[1].present_classs*100)/ s[1].total_class;
+        sub_percentage[1].setText(Integer.toString(per) + "%");
+        sub_count[1].setText(s[1].present_classs+"/"+ s[1].total_class);
 
 
         if(per>=60){
-            s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
+            sub_percentage[1].setBackgroundColor(Color.parseColor("#379237"));
         } else {
-            s1_percentage.setBackgroundColor(Color.parseColor("#ff0000"));
+            sub_percentage[1].setBackgroundColor(Color.parseColor("#ff0000"));
         }
     }
 
 
     //Adding functionality for the button "-" id = Basent
     public void absentButton(View view){
-        t.total_class++;
+        s[1].total_class++;
 
-        int per = (t.present_classs*100)/t.total_class;
-        s1_percentage.setText(Integer.toString(per) + "%");
-        s1_count.setText(t.present_classs+"/"+t.total_class);
+        int per = (s[1].present_classs*100)/ s[1].total_class;
+        sub_percentage[1].setText(Integer.toString(per) + "%");
+        sub_count[1].setText(s[1].present_classs+"/"+ s[1].total_class);
 
 
         if(per>60){
-            s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
+            sub_percentage[1].setBackgroundColor(Color.parseColor("#379237"));
         } else {
-            s1_percentage.setBackgroundColor(Color.parseColor("#ff0000"));
+            sub_percentage[1].setBackgroundColor(Color.parseColor("#ff0000"));
         }
     }
 
@@ -54,20 +55,24 @@ public class Presentance extends AppCompatActivity {
         setContentView(R.layout.activity_presentance);
 
         //Making a teacher Object
-        t  = new Teacher("Advanced Algorithm",0,0);
+        s[0] = new Subject("MicroProcessor",0,0);
+        s[1] = new Subject("Advanced Algorithm",0,0);
+        s[2] = new Subject("Numerical Method",0,0);
+        s[3] = new Subject("Math",0,0);
+        s[4] = new Subject("Economics",0,0);
 
 
         //Getting textview for all ids bt its id
-        s1_name = (TextView)findViewById(R.id.t1);  //Name of Teachar 1 or Subject 1
-        s1_percentage = (TextView)findViewById(R.id.t1p); //Subject 1 Percentage count for Teacher 1
-        s1_count = (TextView)findViewById(R.id.count_1);
+        sub_name[1] = (TextView)findViewById(R.id.t1);  //Name of Teachar 1 or Subject 1
+        sub_percentage[1] = (TextView)findViewById(R.id.t1p); //Subject 1 Percentage count for Subject 1
+        sub_count[1] = (TextView)findViewById(R.id.count_1); //View for 0/0 like presentation
 
 
 
         //Working with Subject 1
-        s1_name.setText(t.name);
-        s1_percentage.setText( "0%");
-        s1_percentage.setBackgroundColor(Color.parseColor("#379237"));
+        sub_name[1].setText(s[1].name);
+        sub_percentage[1].setText( "0%");
+        sub_percentage[1].setBackgroundColor(Color.parseColor("#379237"));
 
 
 
--- app/src/main/java/com/bikash/student/Subject.java
+++ app/src/main/java/com/bikash/student/Subject.java
@@ -3,12 +3,12 @@ package com.bikash.student;
 /**
  * Created by bikash on 8/31/16.
  */
-public class Teacher {
+public class Subject {
     public String name;
     public int total_class;
     public int present_classs;
 
-    Teacher(String n, int t, int p){
+    Subject(String n, int t, int p){
         name = n;
         total_class = t;
         present_classs = p;

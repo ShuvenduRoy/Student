644635d6692070022da7e9d9526a58a0378ddc2e
Bikash <bikash11roy@gmail.com>
2016-09-22 15:53:32 +0600

    Test VCS

--- app/src/main/java/com/bikash/student/MainActivity.java
+++ app/src/main/java/com/bikash/student/MainActivity.java
@@ -21,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-
+        String name = "bikash";
+        
 
 
         //Routine activity

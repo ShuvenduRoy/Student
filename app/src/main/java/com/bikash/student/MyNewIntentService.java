7f558bad5de57f91c5226c44955e2feb99ae9995
Bikash <bikash11roy@gmail.com>
2016-09-23 09:51:32 +0600

    Changing Notification

--- app/src/main/java/com/bikash/student/MyNewIntentService.java
+++ app/src/main/java/com/bikash/student/MyNewIntentService.java
@@ -22,16 +22,27 @@ public class MyNewIntentService extends IntentService {
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onHandleIntent(Intent intent) {
+
         Notification.Builder builder = new Notification.Builder(this);
+
         builder.setContentTitle("Student");
+
         builder.setContentText("Please record today's attendence to class");
+
         builder.setSmallIcon(R.drawable.graduate_icon);
+
         Intent notifyIntent = new Intent(this, Presence.class);
+
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-//to be able to launch your activity from the notification
+
+
+        //to be able to launch your activity from the notification
         builder.setContentIntent(pendingIntent);
+
         Notification notificationCompat = builder.build();
+
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+
         managerCompat.notify(NOTIFICATION_ID, notificationCompat);
     }
 }

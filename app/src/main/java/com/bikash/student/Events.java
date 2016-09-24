95a45f357e845ec629f856a2d59bde019f90a143
Bikash <bikash11roy@gmail.com>
2016-09-25 04:59:11 +0600

    event Time with date and time picker added

--- app/src/main/java/com/bikash/student/Events.java
+++ app/src/main/java/com/bikash/student/Events.java
@@ -1,25 +1,35 @@
 package com.bikash.student;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashSet;
 
 public class Events extends AppCompatActivity implements TextWatcher {
     EditText editText;
     int noteId;
+    String time;
+
+    Calendar calendar;
+    TimePicker timePicker;
+    TextView setDateTextView;
+    int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +37,18 @@ public class Events extends AppCompatActivity implements TextWatcher {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_events);
-        editText = (EditText) findViewById(R.id.editText);
+
+
+        editText = (EditText) findViewById(R.id.EventNameEditView);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        setDateTextView = (TextView) findViewById(R.id.setDateTextView);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
 
         getSupportActionBar().setTitle("Create New Events");
 
@@ -43,7 +64,51 @@ public class Events extends AppCompatActivity implements TextWatcher {
         editText.addTextChangedListener(this);
 
 
+
     }
+
+
+    public void saveTime(View view){
+        int hour = timePicker.getCurrentHour();
+        int min = timePicker.getCurrentMinute();
+        time = hour + ":" + min;
+        Log.i("Time", time);
+
+        String s = setDateTextView.getText() + " " + time + "\n";
+        editText.setText(s + editText.getText());
+
+        finish();
+    }
+
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        setDateTextView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
+
 
     @Override
     public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {

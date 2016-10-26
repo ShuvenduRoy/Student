a2dc2df4f0619c828692443247440d3b9e3cb9a8
Bikash <bikash11roy@gmail.com>
2016-10-26 22:44:13 +0600

    Event List added

--- app/src/main/java/com/bikash/student/BasicEventsActivity.java
+++ app/src/main/java/com/bikash/student/BasicEventsActivity.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,34 +31,38 @@ public class BasicEventsActivity extends AppCompatActivity {
     ListView listView;
     static ArrayList<String> events;
     static ArrayAdapter arrayAdapter;
-    //static Set<String> set;
-    //SharedPreferences sharedPreferences;
-    SQLiteDatabase mydatabase;
-    private DatabaseReference mFirebaseDatabaseReference;
+    private Firebase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /**
+         * Primary setup
+         */
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Toast.makeText(this,"Syncing events...",Toast.LENGTH_LONG).show();
 
-        Firebase.setAndroidContext(this);
-
+        /**
+         * Wiring things up
+         * getting all elemnets
+         */
         setContentView(R.layout.activity_basic_events);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("UpComing Events");
-
-        Toast.makeText(this,"Syncing events...",Toast.LENGTH_LONG).show();
-
-
-
-
-        mydatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
-        events = new ArrayList<>();
-
-
         listView = (ListView) findViewById(R.id.listView);
+
+
+        /**
+         * Initializing variable and data base connection
+         */
+        firebaseDatabase = new Firebase("https://student-eaf3d.firebaseio.com/events/"+HomeActivity.userGroup);
+        events = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, events);
+        listView.setAdapter(arrayAdapter);
+        firebaseDatabase.keepSynced(true);
+
 
 
         /**
@@ -65,12 +70,12 @@ public class BasicEventsActivity extends AppCompatActivity {
          * onchildAdded listener will keep arraylist updated
          */
 
-        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
-        mFirebaseDatabaseReference.keepSynced(true);
 
-        mFirebaseDatabaseReference.child("events").child(HomeActivity.userGroup).addChildEventListener(new ChildEventListener() {
+
+
+        firebaseDatabase.addChildEventListener(new com.firebase.client.ChildEventListener() {
             @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            public void onChildAdded(com.firebase.client.DataSnapshot dataSnapshot, String s) {
                 Event e = dataSnapshot.getValue(Event.class);
 
                 String eventadded = new String(e.getDate() + " " + e.getTime() + "\n" + e.getEvent());
@@ -78,73 +83,29 @@ public class BasicEventsActivity extends AppCompatActivity {
                 events.add(eventadded);
                 Collections.sort(events);
                 arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(com.firebase.client.DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            public void onChildRemoved(com.firebase.client.DataSnapshot dataSnapshot) {
 
             }
 
             @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            public void onChildMoved(com.firebase.client.DataSnapshot dataSnapshot, String s) {
 
             }
 
             @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(FirebaseError firebaseError) {
 
             }
         });
 
-
-
-
-
-
-        /**
-         * This try block loads all data from local data base
-         * but if we start using firebase data base adding this data will duplicate our data
-         */
-
-//        try{
-//            mydatabase.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR)");
-//
-//            Cursor c = mydatabase.rawQuery("SELECT * FROM events", null);
-//
-//            int eventIndex = c.getColumnIndex("event");
-//
-//            c.moveToFirst();
-//            while(c!=null){
-//
-//                String evnetName = c.getString(eventIndex);
-//                c.moveToNext();
-//
-//                events.add(evnetName);
-//                Log.i("Event", evnetName);
-//            }
-//        } catch (Exception e){
-//
-//            e.printStackTrace();
-//
-//        }
-
-        Collections.sort(events);
-
-
-
-
-
-
-
-
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, events);
-        listView.setAdapter(arrayAdapter);
 
 
         /**
@@ -191,17 +152,7 @@ public class BasicEventsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                /*
-                 IT was required in local data base system to add a new and send its id to intend
-                 but not requird in server bases system
-                 */
-                //events.add("");
-
-
-
                 Intent i = new Intent(getApplicationContext(), Events.class);
-                //i.putExtra("Id", events.size()-1);
                 startActivity(i);
 
             }
@@ -211,26 +162,6 @@ public class BasicEventsActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy() {
-
-        /**
-         *
-         * Nothing of this is required after adding server
-         */
-
-//        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS events (event VARCHAR)" );
-//
-//        mydatabase.delete("events",null,null);
-//
-//
-//
-//        for(int i=0; i<events.size(); i++){
-//
-//            String e = events.get(i);
-//            String sql = "INSERT INTO events (event) VALUES ('"+e+"') ";
-//            mydatabase.execSQL(sql);
-//
-//        }
-
         super.onDestroy();
     }
 }

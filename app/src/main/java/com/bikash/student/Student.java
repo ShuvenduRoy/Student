bee15c49149c27aa4d436a8482fc432adeef0531
Bikash <bikash11roy@gmail.com>
2016-10-26 21:05:00 +0600

    Stable Student

--- app/src/main/java/com/bikash/student/Student.java
+++ app/src/main/java/com/bikash/student/Student.java
@@ -2,6 +2,7 @@ package com.bikash.student;
 
 import android.app.Application;
 
+import com.firebase.client.Firebase;
 import com.google.firebase.database.FirebaseDatabase;
 
 
@@ -14,6 +15,7 @@ public class Student extends Application {
     public void onCreate() {
         super.onCreate();
 
+        Firebase.setAndroidContext(this);
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
 
     }
